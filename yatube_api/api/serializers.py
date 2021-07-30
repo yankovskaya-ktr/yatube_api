8f38a49f04570bc33d44fca5417d292a23405288
Ekaterina Yankovskaya <yankovskaya.ktr@yandex.ru>
2021-07-30 23:18:23 +0300

    api yatube

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
 
     class Meta:
         fields = '__all__'
@@ -20,4 +23,38 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         fields = '__all__'
+        read_only_fields = ('author', 'post')
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username', read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = SlugRelatedField(
+        slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя')
+        return data
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+router.register('v1/posts', PostViewSet)
+router.register('v1/groups', GroupViewSet)
+router.register('v1/follow', FollowViewSet, basename='comment')
+router.register(r'v1/posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comment')
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
